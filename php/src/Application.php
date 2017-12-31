a223e349198652f136d1a48d8645b79bc0ed3e56
Cscharwitzl <clemens@scharwitzl.com>
2017-12-31 13:18:47 +0100

    debug Application php

--- php/src/Application.php
+++ php/src/Application.php
@@ -46,13 +46,7 @@ class Application implements  MessageComponentInterface {
 
         try{
 
-            echo "1\n";
-
             $commands = json_decode($msg, true);
-
-            echo "2\n";
-
-            print_r($commands);
 
             print_r(JWT::decode($commands['jwt'],$this->key, array('HS512')));
 
@@ -89,7 +83,7 @@ class Application implements  MessageComponentInterface {
         }catch (\Exception $ex){
             $from->send('Wrong or no jwt');
             //$from->close();
-            echo 'There was an Error: '.$ex->getMessage().' '.$ex->getCode()."\n";
+            echo 'There was an Error: '.$ex->getMessage().' '.$ex->getCode().' '.$ex->getLine().' '.$ex->getFile()."\n";
             echo $this->key;
         }
 
