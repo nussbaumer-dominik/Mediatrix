802520865b0c9fc6075c048f31fa7d3f26e2a72d
nussi98 <nussbaumer.domi@gmail.com>
2018-01-22 19:13:19 +0100

    changed most of the dashboard and mockups

---
+++ js/vue_app.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        p1: "0",
+        p2: "0",
+        p3: "0",
+        p4: "0",
+        p5: "0"
+    },
+    methods: {
+        log: function(message) {
+            console.log(message)
+        }
+    }
+})
