21ebb3152ec7b087ad34b047349436b33c785326
Cscharwitzl <clemens@scharwitzl.com>
2018-01-28 12:15:43 +0100

    add on and off to RGBScheinwerfer

--- php/src/RGBWScheinwerfer.php
+++ php/src/RGBWScheinwerfer.php
@@ -33,4 +33,50 @@ class RGBWScheinwerfer extends Scheinwerfer
         return $r;
     }
 
+    function off()
+    {
+        $data = array(
+          $this->channels['r'] => 255,
+          $this->channels['g'] => 255,
+          $this->channels['b'] => 255
+        );
+
+        if(count($this->channels) == 4){
+            $data[$this->channels['w']] = 255;
+        }
+
+        return json_decode(
+            str_replace("'", '"',
+                $this->dmx::sendChannel(
+                    array(
+                        $data
+                    )
+                )
+            )
+        );
+    }
+
+    function on()
+    {
+        $data = array(
+            $this->channels['r'] => 0,
+            $this->channels['g'] => 0,
+            $this->channels['b'] => 0
+        );
+
+        if(count($this->channels) == 4){
+            $data[$this->channels['w']] = 0;
+        }
+
+        return json_decode(
+            str_replace("'", '"',
+                $this->dmx::sendChannel(
+                    array(
+                        $data
+                    )
+                )
+            )
+        );
+    }
+
 }
