600962fbc43728fe044dbfe97828efa061508025
Cscharwitzl <clemens@scharwitzl.com>
2018-01-28 19:55:29 +0100

    add test.php

--- php/test.php
+++ php/test.php
@@ -6,13 +6,17 @@
  * Time: 19:52
  */
 
+echo "Test: \n";
+
 $sqlite = new \SQLite3("../sqlite/db.sqlite");
 
 $stm = $sqlite->prepare('SELECT * FROM preset');
 
 $result = $stm->execute();
 
-    while($res = $result->fetchArray(SQLITE3_ASSOC)){
+var_dump($result->numColumns());
+
+ while($res = $result->fetchArray(SQLITE3_ASSOC)){
 
         print_r($res);
     }
