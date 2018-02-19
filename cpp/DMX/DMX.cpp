a51862d5296abc25c66bc2de8bd47e22b8d76ebf
Cscharwitzl <clemens@scharwitzl.com>
2018-02-19 22:44:25 +0100

    change dmx channels

--- cpp/DMX/DMX.cpp
+++ cpp/DMX/DMX.cpp
@@ -72,7 +72,7 @@ class DMX : public Php::Base {
             ola::DmxBuffer buffer; // A DmxBuffer to hold the data.
             buffer.Blackout(); // Set all channels to 0
 
-            for(unsigned int i = 0; i < 5121; i++){
+            for(unsigned int i = 0; i < 512; i++){
 
                 DMX::channels[i] = 0;
             }
