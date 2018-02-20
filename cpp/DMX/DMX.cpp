2e10fd2c73c5d690de9e38522c16df8215c7c0fa
Cscharwitzl <clemens@scharwitzl.com>
2018-02-20 10:56:03 +0100

    change dmx channels

--- cpp/DMX/DMX.cpp
+++ cpp/DMX/DMX.cpp
@@ -20,7 +20,7 @@ class DMX : public Php::Base {
     private:
         static const unsigned int UNIVERSE = 0; // UNIVERSE to use for sending data
 
-        enum int[512] { channel = {}};
+        enum Channels { int channel[512] = {}};
 
         //static ola::DmxBuffer buffer = new ola::DmxBuffer;
 
