7d81d7e607bd4a2e25468e05307b729ec6aa33d6
Cscharwitzl <clemens@scharwitzl.com>
2018-02-27 10:47:50 +0100

    fix readCode

--- php/src/readCode.php
+++ php/src/readCode.php
@@ -5,33 +5,58 @@
  * Date: 01.02.2018
  * Time: 20:31
  */
+namespace Mediatrix;
 
 echo "Reading IR-Code";
 
-$ir = new \IR();
+class readCode{
+    private $ir;
 
-echo "Modes:\n";
+    public function __construct()
+    {
+        $this->ir = new \IR();
+    }
 
-$modes = $ir->getMode();
+    function modes()
+    {
+        echo "Modes:\n";
 
-if(!strlen($modes) > 0){
-    echo "IR-Device is bussy. Try again later, or disconnect and reconnect the device.\n";
-    return;
+        $modes = $this->ir::getMode();
+
+        if (!strlen($modes) > 0) {
+            throw new \Exception("IR-Device is bussy. Try again later, or disconnect and reconnect the device.\n");
+        }
+
+        $modesArray = explode("\\", $modes);
+
+        for ($i = 1; $i <= count($modesArray); $i++) {
+            printf("%d.......%s\n", $i, $modesArray[$i - 1]);
+        }
+
+        $readMode = readline("Which Mode: ");
+
+        return $readMode;
+
+    }
+
+    function read($readMode)
+    {
+        $code = $this->ir::read($readMode);
+
+        while (strlen($code) < 0) {
+            readline("No code found. Press Enter to try again or Crtl+C to abroad.");
+            $code = $this->ir::read($readMode);
+        }
+
+        return $code;
+    }
+
 }
-
-$modesArray = explode("\\",$modes);
-
-for($i = 1; $i<=count($modesArray); $i++){
-    printf("%d.......%s\n",$i,$modesArray[$i-1]);
+try {
+    $class = new readCode();
+    $mode = $class->modes();
+    $class->read($mode);
 }
-
-$readMode =  readline("Which Mode: ");
-
-$code = $ir->read($readMode);
-
-while(strlen($code) < 0){
-    readline("No code found. Press Enter to try again or Crtl+C to abroad.");
-    $code = $ir->read($readMode);
+catch (\Exception $ex){
+    echo $ex->getMessage();
 }
-
-echo $code;
