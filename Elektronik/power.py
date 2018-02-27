fe1c17977d7774ea5c468cfceed6b566b7438b9b
Flo <florianst678@gmail.com>
2018-02-27 15:07:04 +0100

    Einschalten Methode

--- Elektronik/power.py
+++ Elektronik/power.py
@@ -1,3 +1,40 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+import time
+import RPi.GPIO as GPIO
+
+# SET GPIO Button-Pin
+btn = 15
+panel = 7
+door = 8
 
 #Einschalten
+
+def button():
+    value = 0
+
+    while True:
+
+        if not GPIO.input(btn):
+            value += 0.01
+
+        if value > 0:
+
+            if GPIO.input(btn):
+                print "gedrueckt"
+                button()
+
+        time.sleep(0.03)
+
+    return 0
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(btn, GPIO.IN)
+    button()
+
+
+
