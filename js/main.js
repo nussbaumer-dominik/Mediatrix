0b6bbc94f2342a592c8aab83e25f003a1824da21
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-03-10 16:31:46 +0100

    dev

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ window.onload = function() {
   //wird bei response des Servers ausgegeben
   socket.onmessage = function(event) {
     console.log("message: " + event.data);
-    if(event.data.ini){
+    if(event.data.contains("ini")){
       console.log("Das ist der ini-String");
     }
   };
