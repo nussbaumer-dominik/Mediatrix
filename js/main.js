d12896c2ed11342ef2b1f61a68a0341161beaafc
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-03-13 16:37:47 +0100

    dev

--- js/main.js
+++ js/main.js
@@ -175,7 +175,7 @@ window.onload = function() {
         console.log(scheinwerfer.id);
         var cont = document.querySelector('#rgbwTemplate');
         console.log(cont+"");
-        cont.content.querySelectorAll('.slider').setAttribute("data-id", scheinwerfer.id);
+        cont.content.querySelector('.slider').setAttribute("data-id", scheinwerfer.id);
         $(".flex-container").append(cont);
         //$("#rgbwTemplate").find(".slider").attr('data-id', scheinwerfer.id);
       }else if(scheinwerfer.numberChannels == "1"){
