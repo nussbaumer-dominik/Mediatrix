f36ca052b3a4a57349ac7ac8c59efd70839b6609
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-03-17 23:52:51 +0100

    preset dev

--- js/main.js
+++ js/main.js
@@ -367,7 +367,8 @@ $(function() {
 
   function getPresets(){
     for(let i=0;i<Object.keys(presets).length;i++){
-      console.log(presets[i].name + " conf:" + presets[i].conf);
+      console.log(presets[i].name + " conf:");
+      console.log(presets[i].conf);
       /*for(let j=0;i<Object.keys(presets[i].conf).length;j++){
         console.log(j);
       }*/
