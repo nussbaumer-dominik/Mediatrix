3323630351218cee4b02a50bb94865848a640645
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-03-23 22:39:17 +0100

    updated websocket address

--- js/main.js
+++ js/main.js
@@ -14,7 +14,8 @@ $(function() {
             "beamer": {}
         };
 
-    socket = new WebSocket('wss://10.0.0.121/wss');
+    //socket = new WebSocket('wss://10.0.0.121/wss');
+    socket = new WebSocket('wss://10.0.0.144/wss');
 
     //wirft eine Exception
     socket.onerror = function(error) {
