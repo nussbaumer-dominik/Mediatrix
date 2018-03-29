cc3643e2059edfb581815629003d7b3db03ea707
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-03-29 22:20:18 +0200

    Mixer dev

--- php/src/Mixer.php
+++ php/src/Mixer.php
@@ -33,7 +33,6 @@ class Mixer {
 
 		try {
 			$conn = new Client("ws://" . $ipAddress . "/socket.io/1/websocket/" . $session_id);
-			$conn->send("Hallo");
 
 			echo $conn->receive(); 
 		}catch (Exception $ex){
@@ -56,7 +55,7 @@ class Mixer {
 
 	public function alive() {
 		echo "Alive\n";
-		//$this->$conn->send($alive);
+		$this->$conn->send($alive);
 	}
 
 	public function setLineVolume($val) {
