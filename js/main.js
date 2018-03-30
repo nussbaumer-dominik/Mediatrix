bc220cbb108711b856a2ff4307338c3fec11faa8
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-03-30 18:49:14 +0200

    Mixer dev

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ $(function() {
 					console.log(
 						"Dieser Slider ist von einem Mixer: " + slider.get()
 					);
-					var data = { mixer = [] };
+					var data = { mixer: [] };
 
 					//var mixer = [];
 					/*mixer.mikrofone = {
