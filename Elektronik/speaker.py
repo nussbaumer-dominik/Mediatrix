d298a310b95be732588480c29ddeeaaa72fec98f
Cscharwitzl <clemens@scharwitzl.com>
2018-03-30 12:03:57 +0200

    python dev

---
+++ Elektronik/speaker.py
@@ -0,0 +1,58 @@
+#!usr/bin/python
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)			#disable warnings
+
+GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
+
+# SET GPIO Button-Pin
+door = 8
+
+#Relais
+rs = 2 #Relais Fur Lautsprecher
+rp = 3 #Relais fur Strom //momentan fur test
+
+# GPIO Modus zuweisen
+GPIO.setup(door, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+GPIO.setup(rs, GPIO.OUT)
+GPIO.setup(rp, GPIO.OUT)
+
+#Einschalten
+
+pstate = 0 #Zustand des Systems Strom (1=Ein, 0=Aus)
+sstate = 0 #Zustand des Systems Lautsprecher (1=Ein, 0=Aus)
+
+
+#Lautsprecher schalten
+def speakerOn():
+    global sstate
+
+    print "changing Speaker-State"
+    print sstate
+
+    GPIO.output(rs, GPIO.HIGH) # an
+    sstate = 1
+
+
+def speakerOff():
+    global sstate
+
+    print "changing Speaker-State"
+    print sstate
+
+    GPIO.output(rs, GPIO.LOW) # aus
+    sstate = 0
+
+if __name__ == '__main__':
+
+    while True:
+        if sstate == 0:
+            GPIO.wait_for_edge(door, GPIO.RISING)
+            speakerOn()
+
+        if sstate == 1:
+            GPIO.wait_for_edge(door, GPIO.FALLING)
+            speakerOff()
