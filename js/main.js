69d7f274ef5604e7503922afb182cbc63fdbb4c8
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-04-02 13:20:22 +0200

    Preset dev

--- js/main.js
+++ js/main.js
@@ -93,28 +93,23 @@ $(function() {
 					var obj = { id: id, value: val };
 					mixerData.mixer.mikrofone = [obj];
 					for (
-						let i = 1;
+						let i = 0;
 						i <= Object.keys(conf.mixer.mikrofone).length;
 						i++
 					) {
-						let j = i - 1;
-						console.log("i: " + j);
-						console.log(
-							"erstes Element des Conf Objekts ohne Index: "
-						);
-						console.log(conf.mixer.mikrofone[0]);
+						console.log("i: " + i);
 						console.log(
 							"Id des Elements " +
-								j +
+								i +
 								" des Conf Objekts mit index: "
 						);
-						console.log(conf.mixer.mikrofone[j].id);
+						console.log(conf.mixer.mikrofone[i].id);
 
-						if (conf.mixer.mikrofone[j].id == "") {
-							conf.mixer.mikrofone[j].id = id;
-							conf.mixer.mikrofone[j].value = val;
-						} else if (conf.mixer.mikrofone[j].id == id) {
-							conf.mixer.mikrofone[j].value = val;
+						if (conf.mixer.mikrofone[i].id == "") {
+							conf.mixer.mikrofone[i].id = id;
+							conf.mixer.mikrofone[i].value = val;
+						} else if (conf.mixer.mikrofone[i].id == id) {
+							conf.mixer.mikrofone[i].value = val;
 						}
 					}
 
