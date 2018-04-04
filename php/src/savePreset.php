3f279f5b2314447ced4dec869d04cae901a3da45
Cscharwitzl <clemens@scharwitzl.com>
2018-04-04 19:34:02 +0200

    debug

--- php/src/savePreset.php
+++ php/src/savePreset.php
@@ -13,7 +13,7 @@ preg_match('/^[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+)?$/', $_POST[
 
 preg_match('/^[0-9A-Za-z_-]+$/', $_POST['name']) or die('{"success":false,"err","name not valid"}');
 
-json_decode($_POST['data']);
+json_decode($_POST['conf']);
 json_last_error() == JSON_ERROR_NONE or die('{"success":false,"err":"json not valid"}');
 
-echo Preset::create($_POST['name'],$_POST['data'],$_POST['jwt']);
+echo Preset::create($_POST['name'],$_POST['conf'],$_POST['jwt']);
