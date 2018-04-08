fb1cd75156f08b93484b87dc6e69b68b44b3335f
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-04-08 17:53:46 +0200

    websocket dev

--- php/src/Mixer.php
+++ php/src/Mixer.php
@@ -7,7 +7,8 @@ class Mixer {
 	//Variablen
 	protected $session_id;
 	protected $mixer;
-	protected $lastVolume;
+	protected $count = 1;
+	protected $lastVolume = 0.0;
 	protected $command = "3:::SETD^i.";
 
 	//Konstruktor
@@ -76,11 +77,15 @@ class Mixer {
 
 	public function alive() {
 		try {
-			echo "Alive " . "3:::ALIVE + 2::\n";
-			$this->mixer->send("3:::ALIVE");
+			echo "Alive " . "2::\n";
 			$this->mixer->send("2::");
-			$this->mixer->send("3:::SETD^i.0.mix^" . $this->lastVolume);
+			//$this->mixer->send("3:::SETD^i.0.mix^" . $this->lastVolume);
 			echo "3:::SETD^i.0.mix^" . $this->lastVolume. "\n";
+			if($this->count % 10 == 1){
+				echo "3:::ALIVE\n";
+				$this->mixer->send("3:::ALIVE");
+			}
+			$this->count++;
 			return array("success" => true, "err" => "");
 		} catch(Exception $ex) {
 			return array("success" => false, "err" => $ex);
