e8416d393f23486ea899af86a5188b7bdeabbc9e
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-04-08 10:45:23 +0200

    looop

--- php/src/MyIoServer.php
+++ php/src/MyIoServer.php
@@ -24,7 +24,7 @@ class MyIoServer extends \Ratchet\Server\IoServer
         $loop = LoopFactory::create();
 
 
-        $loop->addPeriodicTimer(3, function() use (&$mixer) {
+        $loop->addPeriodicTimer(4, function() use (&$mixer) {
             $mixer->alive();
         });
 
