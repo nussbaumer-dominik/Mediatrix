9915113a3b1c28aa6d556dabf115a40cad3f025c
Dominik Nußbaumer <nussbaumer.domi@gmail.com>
2018-04-13 15:04:13 +0200

    outputMessage

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ $(function () {
 			socket.send('{"jwt":"' + jwt + '","ini":1}');
 		}
 		console.log("socket open: " + socket + " " + event);
-		outputMessage(event.data);
+		outputMessage("Die WebSocket-Verbindung konnte erfolgreich geöffnet werden");
 	};
 
 	//wird bei Response des Servers ausgelöst
