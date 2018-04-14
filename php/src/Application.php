52b92836e91fed973df6debc5423e66f336bf22a
Cscharwitzl <clemens@scharwitzl.com>
2018-04-14 13:48:49 +0200

    add user to group

--- php/src/Application.php
+++ php/src/Application.php
@@ -109,9 +109,14 @@ class Application implements MessageComponentInterface
                 */
                 if (isset($commands['group'])){
                     if(isset($commands['group']['register']) && isset($this->forRegister[$commands['group']['register']])){
-                        $this->group->addUser($this->forRegister[$commands['group']['register']]['conn']);
+
+                        $user = $this->forRegister[$commands['group']['register']];
+
+                        $this->group->addUser($user['conn']);
+
+                        $user['conn']->send(json_encode($this->addLiveStatus($this->getIniString($user['username']))));
+
                         unset($this->forRegister[$commands['group']['register']]);
-                        $from->send(json_encode($this->addLiveStatus($this->getIniString($this->forRegister[$commands['group']['register']]['username']))));
                     }else{
                         array_push($result, json_decode('{"success":false,"err":"Connection Id not valid"}'));
                     }
