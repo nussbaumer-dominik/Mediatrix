091c346e9770650f548f2c6eef3fc3eac36785d1
Cscharwitzl <clemens@scharwitzl.com>
2018-05-13 10:58:22 +0200

    check.sh dev

--- Daemon/check.sh
+++ Daemon/check.sh
@@ -11,13 +11,19 @@ DAEMON_OPTS="/var/www/html/Mediatrix/php/src/websocket.php"
 START_OPTS="--start --background --make-pidfile --pidfile ${PIDFILE} --chuid www-data --chdir /var/www/html/Mediatrix/php --exec ${DAEMON} ${DAEMON_OPTS}"
 STOP_OPTS="--stop --pidfile ${PIDFILE}"
 
+shopt -s nullglob
+array=(/var/run/mediatrix*)
+
 while true
 do
-    if [[ $(sudo kill -0 $(cat $1)) ]];  then
-        $(/sbin/start-stop-daemon $START_OPTS >> $LOGFILE)
-    else
-        echo "running"
-    fi
+    for i in ${array[@]}; do
+        echo $i;
+        if [[ $(sudo kill -0 $(cat $i)) ]];  then
+            $(/sbin/start-stop-daemon $START_OPTS >> $LOGFILE)
+        else
+            echo "running"
+        fi
+    done;
     sleep 10
 
 done
