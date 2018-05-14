35059087530d9d4f5a2858342c584f3600f5199c
Cscharwitzl <clemens@scharwitzl.com>
2018-05-14 14:51:07 +0200

    check.sh and daemon dev

--- Daemon/check.sh
+++ Daemon/check.sh
@@ -4,11 +4,14 @@ set -x
 
 NAME=Mediatrix
 DESC="Daemon for the mediatrix websocket server"
-PIDFILE="/var/run/${NAME}.pid"
+PIDFILE="/var/run/${NAME}-server.pid"
 LOGFILE="/var/log/${NAME}.log"
 
 DAEMON="/usr/bin/php"
 DAEMON_OPTS="/var/www/html/Mediatrix/php/src/websocket.php"
+
+PYTHON_OPTS="--start --background --make-pidfile --chdir /var/www/html/Mediatrix/Elektronik"
+PYTHON="/usr/bin/python"
 
 START_OPTS="--start --background --make-pidfile --pidfile ${PIDFILE} --chuid www-data --chdir /var/www/html/Mediatrix/php --exec ${DAEMON} ${DAEMON_OPTS}"
 STOP_OPTS="--stop --pidfile ${PIDFILE}"
@@ -23,18 +26,26 @@ do
         if [ -z "$(ps -p $(cat $i) -o pid=)" ];  then
             case "$(echo $i | cut -d '-' -f 2 | cut -d '.' -f 1)" in
             speaker)
+                echo -n "Restarting speaker.py: " >> $LOGFILE
+	            /sbin/start-stop-daemon $PYTHON_OPTS --pidfile "/var/run/Mediatrix-speaker.pid" --exec $PYTHON speaker.py
                 echo $i;
             ;;
             cooling)
+                echo -n "Restarting cooling.py " >> $LOGFILE
+	            /sbin/start-stop-daemon $PYTHON_OPTS --pidfile "/var/run/Mediatrix-cooling.pid" --exec $PYTHON cooling.py
                 echo $i;
             ;;
             power)
+                echo -n "Restarting power.py: " >> $LOGFILE
+                /sbin/start-stop-daemon $PYTHON_OPTS --pidfile "/var/run/Mediatrix-power.pid" --exec $PYTHON power.py
                 echo $i;
             ;;
             websocket)
                 echo $i;
             ;;
             server)
+                echo -n "Restarting ${DESC}: Server" >> $LOGFILE
+                /sbin/start-stop-daemon $START_OPTS >> $LOGFILE
                 echo $i;
             ;;
             esac
