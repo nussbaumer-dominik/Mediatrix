6f3aa30a0f1f591a59981fad9fe014640c9e29f3
Cscharwitzl <clemens@scharwitzl.com>
2018-07-18 10:50:20 +0200

    debug php

--- php/src/mixerLoop.php
+++ php/src/mixerLoop.php
@@ -5,12 +5,12 @@
  * Date: 12.05.2018
  * Time: 23:13
  */
-/*
+
 use React\EventLoop\Factory as LoopFactory;
 use Mediatrix\Mixer;
 
 require __DIR__ . '/../vendor/autoload.php';
-
+/*
 $mixer = new \Mediatrix\Mixer('192.168.1.100');
 //$mixer = new \Mediatrix\Mixer('10.0.0.53');
 //$mixer = null;
@@ -19,14 +19,13 @@ $GLOBALS['mixer'] = 2;
 
 
 var_dump($GLOBALS['mixer']);
-/*
+
 $loop = LoopFactory::create();
 
 $loop->addPeriodicTimer(1, function() {
     /*if(isset($GLOBALS['mixer']) && !is_null($GLOBALS['mixer'])) {
         $GLOBALS['mixer']->alive();
-    }
+    }*/
 });
 
 $loop->run();
-*/
